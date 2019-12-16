a3571a4bfae81b2bebcd46528041c83a83892aba
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-12-16 15:10:07 +0530

    Migrated run commands to shell script

---
+++ scripts/startApplication.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export JAVA_TOOL_OPTIONS="-Xmx256m -Xms64m -Xss228k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8"
+./gradlew clean build bootJar
+java -Dserver.port=$PORT $JAVA_TOOL_OPTIONS -jar build/libs/userprofile-1.0.0-SNAPSHOT.jar
