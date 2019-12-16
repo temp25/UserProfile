868c6ae126df312f01b4ed9836da2ac92275b427
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-12-16 15:15:27 +0530

    Removed clean build in gradle tasks

--- scripts/startApplication.sh
+++ scripts/startApplication.sh
@@ -2,5 +2,5 @@
 
 export JAVA_TOOL_OPTIONS="-Xmx256m -Xms64m -Xss228k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8"
 chmod +x gradlew
-./gradlew clean build bootJar
+./gradlew bootJar
 java -Dserver.port=$PORT $JAVA_TOOL_OPTIONS -jar build/libs/userprofile-1.0.0-SNAPSHOT.jar
