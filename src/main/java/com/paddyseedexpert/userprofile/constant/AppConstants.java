86064ed9f84ec754e183176257fdd4b7ca678e83
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-12-29 20:34:35 +0530

    Added few more constants for the app

--- src/main/java/com/paddyseedexpert/userprofile/constant/AppConstants.java
+++ src/main/java/com/paddyseedexpert/userprofile/constant/AppConstants.java
@@ -7,5 +7,16 @@ public class AppConstants {
 	public static final String FETCH_ACCESS_TOKEN = "Ckvl12rHQLy3iHVPvxzh0Q";
 	public static final String DELETE_ACCESS_TOKEN = "XHem2NHrSVWHm5VW8LtfFQ";
 	public static final String AUTH_ACCESS_TOKEN = "aDZbMiroR6C6o2hpgRKAzg";
+	public static final String RESET_ACCESS_TOKEN = "G9Cas9iTQFW4av1ab4Js5A";
+	public static final String RETRIEVE_ACCESS_TOKEN = "RzcA8f02RciGorjhOY2lTQ";
+	
+	public static final String FROM_ADDRESS = "no-reply@cocypher.com";
+	public static final String ERROR = "error";
+	public static final String ERROR_CODE = "errorCode";
+	public static final String SUCCESS = "success";
+	public static final String TIMESTAMP_FORMAT = "HH:mm:ss.SSS dd-MM-yyyy zzz";
+	
+	public static final String TURBO_SMTP_AUTH_USERNAME = System.getenv("TURBO_SMTP_AUTH_USERNAME");
+	public static final String TURBO_SMTP_AUTH_PASSWORD = System.getenv("TURBO_SMTP_AUTH_PASSWORD");
 	
 }
