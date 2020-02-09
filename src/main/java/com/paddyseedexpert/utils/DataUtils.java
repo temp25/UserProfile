c840dda827950381902313a91adb5666ec130578
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2020-02-10 03:34:45 +0530

    Removed JSON indent feature of Jackson

--- src/main/java/com/paddyseedexpert/utils/DataUtils.java
+++ src/main/java/com/paddyseedexpert/utils/DataUtils.java
@@ -1,18 +1,17 @@
 package com.paddyseedexpert.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.paddyseedexpert.userprofile.model.User;
-
 import static com.paddyseedexpert.userprofile.constant.AppConstants.ERROR;
 import static com.paddyseedexpert.userprofile.constant.AppConstants.ERROR_CODE;
 import static com.paddyseedexpert.userprofile.constant.AppConstants.TIMESTAMP_FORMAT;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.paddyseedexpert.userprofile.model.User;
 
 public class DataUtils {
 
@@ -31,13 +30,11 @@ public class DataUtils {
 
 	private static String getJSON(Object object) throws JsonProcessingException {
 		ObjectMapper mapper = new ObjectMapper();
-		mapper.enable(SerializationFeature.INDENT_OUTPUT);
 		return mapper.writeValueAsString(object);
 	}
 
 	public static JsonNode stringToJSON(String mailResponse) throws JsonMappingException, JsonProcessingException {
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readTree(mailResponse);
+		return new ObjectMapper().readTree(mailResponse);
 	}
 	
 	public static String getMailerErrorMessage(JsonNode response) {
